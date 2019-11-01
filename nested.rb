501fce62f6f0d7c72e5471217fd61862f1db6507
Tissiana Alves <altissiana@gmail.com>
2019-11-01 00:26:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,12 +73,9 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
         }
      }
+     programmer_hash
   programmer_hash
 end
 
